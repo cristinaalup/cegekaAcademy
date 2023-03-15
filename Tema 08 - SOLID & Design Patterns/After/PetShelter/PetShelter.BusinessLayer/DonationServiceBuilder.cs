a01ec46ec67960a12ae73ad28f00de6c8cd08caf
cristinaalup <cristinalupascu418@gmail.com>
2023-03-15 23:36:28 +0200

    Add DonationServiceBuilder

---
+++ Tema 08 - SOLID & Design Patterns/After/PetShelter/PetShelter.BusinessLayer/DonationServiceBuilder.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using PetShelter.BusinessLayer.Tests;
+using PetShelter.DataAccessLayer.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShelter.BusinessLayer
+{
+    public class DonationServiceBuilder
+    {
+        private IDonationRepository _donationRepository;
+        private IValidator<AddDonationRequest> _donationValidator;
+
+        public DonationServiceBuilder WithDonationRepository(IDonationRepository donationRepository)
+        {
+            _donationRepository = donationRepository;
+            return this;
+        }
+
+        public DonationServiceBuilder WithDonationValidator(IValidator<AddDonationRequest> validator)
+        {
+            _donationValidator = validator;
+            return this;
+        }
+
+        public DonationService Build()
+        {
+            if (_donationRepository == null) { throw new ArgumentNullException(nameof(_donationRepository)); }
+            if (_donationValidator == null) { throw new ArgumentNullException(nameof(_donationValidator)); }
+
+            return new DonationService(_donationRepository, _donationValidator);
+        }
+    }
+
+}
