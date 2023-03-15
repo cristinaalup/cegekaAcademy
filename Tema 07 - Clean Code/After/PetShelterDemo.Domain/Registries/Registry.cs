aec7de647e43d4352b12299456ebe2ef7aa8cf66
cristinaalup <cristinalupascu418@gmail.com>
2023-03-15 21:14:36 +0200

    more cleaning

--- Tema 07 - Clean Code/After/PetShelterDemo.Domain/Registries/Registry.cs
+++ Tema 07 - Clean Code/After/PetShelterDemo.Domain/Registries/Registry.cs
@@ -5,30 +5,28 @@ namespace PetShelterDemo.Domain.Registries
 {
     internal sealed class Registry<T> : IRegistry<T> where T : INamedEntity
     {
-        private readonly Database database; // use DB after async await
+        private readonly Database database;
         public Registry(Database database)
         {
             this.database = database;
         }
 
-        public async Task Register(T instance) // Implement this after generic types
+        public async Task Register(T instance) 
         {
             await database.Insert(instance);
         }
 
-        public async Task<IReadOnlyList<T>> GetAll() // Implement this after generic types
+        public async Task<IReadOnlyList<T>> GetAll() 
         {
             return await database.GetAll<T>();
         }
 
-
-
-        public async Task<T> GetByName(string name) // implement after LINQ
+        public async Task<T> GetByName(string name)
         {
             return (await database.GetAll<T>()).Single(o => o.Name == name);
         }
 
-        public async Task<IReadOnlyList<T>> Find(Func<T, bool> filter) // implement after lambda expressions
+        public async Task<IReadOnlyList<T>> Find(Func<T, bool> filter)
         {
             return (await database.GetAll<T>()).Where(filter).ToList();
         }
