058a2d6de39bb0395a3d8658c8c729586feb5e93
cristinaalup <cristinalupascu418@gmail.com>
2023-03-15 20:22:23 +0200

    Add ICustomRegistry

---
+++ Tema 07 - Clean Code/After/PetShelterDemo.Domain/Interfaces/ICustomRegistry.cs
@@ -0,0 +1,8 @@
+﻿namespace PetShelterDemo.Domain.Interfaces;
+
+public interface ICustomRegistry<T>
+{
+    Task<IReadOnlyList<T>> GetAll();
+    Task<T> GetByName(string name);
+    Task Register(T item);
+}
